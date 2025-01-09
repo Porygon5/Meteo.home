4d3e110251b4271cf5f0ad01868528c9efa53e42
unknown <valliermathis.pro@gmail.com>
2025-01-09 21:08:09 +0100

    feat: Ajout d'un système de pagination pour la visualisation des données

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta http-equiv="refresh" content="10">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mesures Météo</title>
+    <title>Meteo.home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
